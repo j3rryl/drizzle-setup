05eb4a537af63fb370daa2817383780d9de045a3
j3rryl <jeremymunroe0@gmail.com>
2024-05-17 15:37:17 +0300

    aixja ri vw nw

--- app/components/sign-in.tsx
+++ app/components/sign-in.tsx
@@ -2,13 +2,24 @@ import { signIn } from "@/auth";
 
 export function SignIn() {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("google");
-      }}
-    >
-      <button type="submit">Signin with Google</button>
-    </form>
+    <>
+      {/* <form
+        action={async () => {
+          "use server";
+          await signIn("google");
+        }}
+      >
+        <button type="submit">Signin with Google</button>
+      </form> */}
+
+      <form
+        action={async () => {
+          "use server";
+          await signIn();
+        }}
+      >
+        <button type="submit">Sign In</button>
+      </form>
+    </>
   );
 }
--- auth.ts
+++ auth.ts
@@ -2,8 +2,22 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 import google from "next-auth/providers/google";
 import { db } from "./db";
+import nodemailer from "next-auth/providers/nodemailer";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [google],
+  providers: [
+    google,
+    nodemailer({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
   adapter: DrizzleAdapter(db),
 });
